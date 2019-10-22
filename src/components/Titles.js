02776031ba3180205d91d4411a7697eb6ffa80d5
Nerajno <Nerajno@gmail.com>
2019-10-22 18:30:15 -0400

    layout in progress

---
+++ src/components/Titles.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Titles extends React.Component {
+    render() {
+        return(
+            <div>
+            <h1>Weather Finder</h1>
+            <p>Find the Weather conditions around you or where ever you like ....</p>
+            </div>
+        );
+    }
+};
+
+export default Titles;
