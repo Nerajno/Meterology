86aa163bc5fa5f8f2da0633065811a75504a0859
Nerajno <Nerajno@gmail.com>
2019-10-26 19:29:20 -0400

    need to update to accommadate zip, auto detected coordinates and input... Need to update user stories

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Weather from "./Components/Weather"
  const cors_api_host = 'https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/f8cbf1d9409a55b2a68c52e994d8777e/37.8267,-122.4233';
 
 class App extends React.Component {
+
     getWeather = async (e) => {
       e.preventDefault();
       // console.log(e);
