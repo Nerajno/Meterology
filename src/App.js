9ce496fb7b2ae3c70251b87cba8cec12c58a7118
Nerajno <Nerajno@gmail.com>
2019-10-25 21:42:44 -0400

    completed the layout

--- src/App.js
+++ src/App.js
@@ -3,9 +3,14 @@ import Titles from "./Components/Titles";
 import Form from "./Components/Form";
 import Weather from "./Components/Weather"
 
-
+const API_KEY = "a80186412681b67855d5994201e8b313";
+const API_URL = "https://openweathermap.org/data/2.5/weather?q=London,uk&";
+const API_UNITS = "&units=metric"
 
 class App extends React.Component {
+    getWeather = () => {
+      const api_call = await fetch(``);
+    }
   render(){
     return(
       <div>
