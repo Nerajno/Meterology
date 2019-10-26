50b92f05598dc205ad522e298fce7bc4c64dd6f8
Nerajno <Nerajno@gmail.com>
2019-10-26 19:04:49 -0400

    having cors issues

--- src/App.js
+++ src/App.js
@@ -3,17 +3,20 @@ import Titles from "./Components/Titles";
 import Form from "./Components/Form";
 import Weather from "./Components/Weather"
 
-const API_KEY = "a80186412681b67855d5994201e8b313";
-const API_URL = "https://openweathermap.org/data/2.5/weather?q=London,uk&appid=";
-const API_UNITS = "&units=metric"
+// const API_KEY = '0e9298136c7c17273ccec159a274dc0f';
+// const cors_api_host = 'cors-anywhere.herokuapp.com/';
+// const cors_api_https = 'https://';
+const API_URL = 'https://api.darksky.net/forecast/f8cbf1d9409a55b2a68c52e994d8777e/37.8267,-122.4233';
+// const API_UNITS = '&units=metric';
 
 class App extends React.Component {
     getWeather = async (e) => {
       e.preventDefault();
       // console.log(e);
-      const api_call = await fetch(`${API_URL}+${API_KEY}+${API_UNITS}`);
+      
+      const api_call = await fetch(`${API_URL}`);
       const data = await api_call.json();
-      console.log(data);
+      console.log(api_call);
       console.log("blue");
       
     }
