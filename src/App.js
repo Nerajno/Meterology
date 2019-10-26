fc31698adeb902e038fd75180f4972d16aeddc67
Nerajno <Nerajno@gmail.com>
2019-10-26 19:11:09 -0400

    fixed cors issues

--- src/App.js
+++ src/App.js
@@ -3,22 +3,17 @@ import Titles from "./Components/Titles";
 import Form from "./Components/Form";
 import Weather from "./Components/Weather"
 
-// const API_KEY = '0e9298136c7c17273ccec159a274dc0f';
-// const cors_api_host = 'cors-anywhere.herokuapp.com/';
-// const cors_api_https = 'https://';
-const API_URL = 'https://api.darksky.net/forecast/f8cbf1d9409a55b2a68c52e994d8777e/37.8267,-122.4233';
-// const API_UNITS = '&units=metric';
+
+ const cors_api_host = 'https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/f8cbf1d9409a55b2a68c52e994d8777e/37.8267,-122.4233';
 
 class App extends React.Component {
     getWeather = async (e) => {
       e.preventDefault();
       // console.log(e);
-      
-      const api_call = await fetch(`${API_URL}`);
+      const api_call = await fetch(`${cors_api_host}`);
       const data = await api_call.json();
       console.log(api_call);
-      console.log("blue");
-      
+      console.log(data);
     }
   render(){
     return(
