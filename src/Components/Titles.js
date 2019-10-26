90bc0d87a922ea7497a3e5c0d4836abd4dcb9c38
Nerajno <Nerajno@gmail.com>
2019-10-25 21:09:04 -0400

    completed the layout

---
+++ src/Components/Titles.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+class Titles extends React.Component {
+    render(){
+        return (
+            <div>
+                <h1> Meterology - React Weather App</h1>
+                <p>Find out the weather conditions where you are or where you are searching for. </p>
+            </div>
+
+        );
+    }
+}
+
+export default Titles;
