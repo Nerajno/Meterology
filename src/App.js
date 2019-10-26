7fce5daeadf17d8ab29eebd77651045d10a10d39
Nerajno <Nerajno@gmail.com>
2019-10-25 23:37:52 -0400

    having cors issues

--- src/App.js
+++ src/App.js
@@ -4,18 +4,24 @@ import Form from "./Components/Form";
 import Weather from "./Components/Weather"
 
 const API_KEY = "a80186412681b67855d5994201e8b313";
-const API_URL = "https://openweathermap.org/data/2.5/weather?q=London,uk&";
+const API_URL = "https://openweathermap.org/data/2.5/weather?q=London,uk&appid=";
 const API_UNITS = "&units=metric"
 
 class App extends React.Component {
-    getWeather = () => {
-      const api_call = await fetch(``);
+    getWeather = async (e) => {
+      e.preventDefault();
+      // console.log(e);
+      const api_call = await fetch(`${API_URL}+${API_KEY}+${API_UNITS}`);
+      const data = await api_call.json();
+      console.log(data);
+      console.log("blue");
+      
     }
   render(){
     return(
       <div>
         <Titles/>
-        <Form/>
+        <Form getWeather={this.getWeather}/>
         <Weather/>
         </div>
     );
