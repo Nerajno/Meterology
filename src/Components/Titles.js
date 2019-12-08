21889973316aea61fbd30258b46e3c71b5ae8c91
Nerajno <Nerajno@gmail.com>
2019-12-08 17:03:49 -0500

    fixed the view

--- src/Components/Titles.js
+++ src/Components/Titles.js
@@ -4,7 +4,7 @@ import React from 'react';
 class Titles extends React.Component {
     render(){
         return (
-            <div>
+            <div className="titleInfo">
                 <h1> Meterology - React Weather App</h1>
                 <p>Find out the weather conditions where you are or where you are searching for. </p>
             </div>
--- src/Components/Weather.js
+++ src/Components/Weather.js
@@ -6,6 +6,7 @@ class Weather extends React.Component {
     render() {
         return (
             <div className="weatherResults">
+                <p>Weather Results:</p>
                 {this.props.city && this.props.country && <p>Location: {this.props.city}, {this.props.country}</p>}
                 { this.props.temperature && <p>Temperature: {this.props.temperature}</p>}
                 {this.props.humidity && <p> Humidity: {this.props.humidity}%</p>}
